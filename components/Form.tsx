1744a0dfd9cfbcb469d33cb5b14b735be9051216
LucasCavalheri <cav.catecumenal@gmail.com>
2023-04-03 19:11:27 -0300

    feat: add Comment and Reply functionality

--- components/Form.tsx
+++ components/Form.tsx
@@ -7,6 +7,8 @@ import React, { useCallback, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import Button from './Button';
 import Avatar from './Avatar';
+import { StringLiteralLike } from 'typescript';
+import usePost from '@/hooks/usePost';
 
 interface FormProps {
   placeholder: string;
@@ -20,7 +22,7 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
 
   const { data: currentUser } = useCurrentUser();
   const { mutate: mutatePosts } = usePosts();
-
+  const { mutate: mutatePost } = usePost(postId as string);
   const [body, setBody] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
@@ -28,18 +30,20 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
     try {
       setIsLoading(true);
 
-      await axios.post('api/posts', { body });
+      const url = isComment ? `/api/comments?postId=${postId}` : '/api/posts';
+      await axios.post(url, { body });
 
       toast.success('Tweet Created');
 
       setBody('');
       mutatePosts();
+      mutatePost();
     } catch (error) {
       toast.error('Something went wrong');
     } finally {
       setIsLoading(false);
     }
-  }, [body, mutatePosts]);
+  }, [body, mutatePosts, mutatePost, isComment, postId]);
 
   return (
     <div className='border-b-[1px] border-neutral-800 px-5 py-2'>
@@ -58,7 +62,11 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
             ></textarea>
             <hr className='opacity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transition' />
             <div className='mt-4 flex flex-row justify-end'>
-              <Button label='Tweet' disabled={isLoading || !body} onClick={onSubmit} />
+              <Button
+                label='Tweet'
+                disabled={isLoading || !body}
+                onClick={onSubmit}
+              />
             </div>
           </div>
         </div>
