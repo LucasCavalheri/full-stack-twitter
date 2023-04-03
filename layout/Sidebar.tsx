730b9e0a0eb25e5cc7b9f3d9429dc0f101e5fee1
LucasCavalheri <cav.catecumenal@gmail.com>
2023-04-03 09:28:14 -0300

    feat: SidebarItem component

--- layout/Sidebar.tsx
+++ layout/Sidebar.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BsBellFill, BsHouseFill } from 'react-icons/bs';
 import { FaUser } from 'react-icons/fa';
+import SidebarLogo from './SidebarLogo';
+import SidebarItem from './SidebarItem';
 
 const Sidebar = () => {
   const items = [
@@ -26,6 +28,14 @@ const Sidebar = () => {
       <div className='flex flex-col items-end'>
         <div className='space-y-2 lg:w-[230px]'>
           <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
         </div>
       </div>
     </div>
