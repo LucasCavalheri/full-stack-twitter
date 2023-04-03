217ebd3ef62022d6faf43a4a053b0dd8ffe2238a
LucasCavalheri <cav.catecumenal@gmail.com>
2023-04-03 19:56:03 -0300

    feat: add Notification functionality

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -23,6 +23,7 @@ const Sidebar = () => {
       href: '/notifications',
       icon: BsBellFill,
       auth: true,
+      alert: currentUser?.hasNotification,
     },
     {
       label: 'Profile',
@@ -44,6 +45,7 @@ const Sidebar = () => {
               label={item.label}
               icon={BiLogOut}
               auth={item.auth}
+              alert={item.alert}
             />
           ))}
           {currentUser && (
