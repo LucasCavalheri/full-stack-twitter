cb7d9f0e88cea8abc530dfcd1c60cf0bbbdc172a
LucasCavalheri <cav.catecumenal@gmail.com>
2023-04-03 18:48:27 -0300

    feat: add Like & Follow functionality

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -1,10 +1,11 @@
+import React, { useCallback, useMemo } from 'react';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import useLoginModal from '@/hooks/useLoginModal';
 import { formatDistanceToNowStrict } from 'date-fns';
+import { AiOutlineHeart, AiFillHeart, AiOutlineMessage } from 'react-icons/ai';
 import { useRouter } from 'next/router';
-import React, { useCallback, useMemo } from 'react';
 import Avatar from '../Avatar';
-import { AiOutlineHeart, AiOutlineMessage } from 'react-icons/ai';
+import useLike from '@/hooks/useLike';
 
 interface PostItemProps {
   data: Record<string, any>;
@@ -16,6 +17,7 @@ const PostItem: React.FC<PostItemProps> = ({ data, userId }) => {
   const loginModal = useLoginModal();
 
   const { data: currentUser } = useCurrentUser();
+  const { hasLiked, toggleLike } = useLike({ postId: data.id, userId });
 
   const goToUser = useCallback(
     (event: any) => {
@@ -34,9 +36,13 @@ const PostItem: React.FC<PostItemProps> = ({ data, userId }) => {
     (event: any) => {
       event.stopPropagation();
 
-      loginModal.onOpen();
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+
+      toggleLike();
     },
-    [loginModal],
+    [loginModal, currentUser, toggleLike],
   );
 
   const createdAt = useMemo(() => {
@@ -44,6 +50,8 @@ const PostItem: React.FC<PostItemProps> = ({ data, userId }) => {
 
     return formatDistanceToNowStrict(new Date(data.createdAt));
   }, [data?.createdAt]);
+
+  const LikeIcon = hasLiked ? AiFillHeart : AiOutlineHeart;
 
   return (
     <div
@@ -72,15 +80,14 @@ const PostItem: React.FC<PostItemProps> = ({ data, userId }) => {
           <div className='flex flex-row items-center mt-3 gap-10'>
             <div className='flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-sky-500'>
               <AiOutlineMessage size={20} />
-              <p>
-                {data.comments?.length || 0}
-              </p>
+              <p>{data.comments?.length || 0}</p>
             </div>
-            <div onClick={onLike} className='flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500'>
-              <AiOutlineHeart size={20} />
-              <p>
-                {data.likes?.length || 0}
-              </p>
+            <div
+              onClick={onLike}
+              className='flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500'
+            >
+              <LikeIcon size={20} color={hasLiked ? 'red' : ''} />
+              <p>{data.likedIds.length}</p>
             </div>
           </div>
         </div>
